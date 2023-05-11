a1898a2212c38a5ca73408e375609dbcba98b147
Toshifumi Kurosawa <kuromailserver@gmail.com>
2023-05-11 21:24:28 +0900

    rm item

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -7,27 +7,6 @@ const apiClient = axios.create({
     baseURL: "http://localhost:8080",
 });
 
-export const createItem = async (name) => {
-    try {
-        const response = await apiClient.post("/items", { name });
-        return response.data;
-    } catch (error) {
-        console.error("Error creating item:", error);
-        throw error;
-    }
-};
-
-export const fetchItems = async () => {
-    console.log("hhhhhhhhhhhhhh fetchItems");
-    try {
-        const response = await apiClient.get("/items");
-        return response.data;
-    } catch (error) {
-        console.error("Error fetching items:", error);
-        throw error;
-    }
-};
-
 export async function registerUser({ name, password, email }) {
     const response = await apiClient.post('/register', { name, password, email });
     return response.data;
