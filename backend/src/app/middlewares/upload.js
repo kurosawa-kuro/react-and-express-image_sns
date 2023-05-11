702a4fd08e939018f26b248ddf66ef60f1f0786e
Toshifumi Kurosawa <kuromailserver@gmail.com>
2023-05-11 18:57:57 +0900

    fix destination

--- backend/src/app/middlewares/upload.js
+++ backend/src/app/middlewares/upload.js
@@ -1,13 +1,16 @@
-// Path: backend/src/middlewares/upload.js
-
 import multer from 'multer';
+import path from 'path';
+import { fileURLToPath } from 'url';
+
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
 
 const storage = multer.diskStorage({
     destination: function (req, file, cb) {
-        cb(null, "../frontend/uploads/")
+        const uploadPath = path.join(__dirname, '../uploads');
+        cb(null, uploadPath);
     },
     filename: function (req, file, cb) {
-        cb(null, Date.now() + '-' + file.originalname)
+        cb(null, Date.now() + '-' + file.originalname);
     }
 });
 
