1962c7a34b9ec4afae8d6194a25c80cec7f10836
Toshifumi Kurosawa <kuromailserver@gmail.com>
2023-05-11 21:21:14 +0900

    add setupDatabaseForOperations

---
+++ backend/src/database/databaseOperations/setupDatabaseForOperations.js
@@ -0,0 +1,15 @@
+import { db } from "../prisma/prismaClient.js";
+import { setupDatabaseForOperations } from "./operations/prepareDatabase.js";
+
+async function main() {
+  await setupDatabaseForOperations();
+
+
+}
+
+main()
+  .catch((e) => console.error(e))
+  .finally(() => {
+    console.log("Finished database operations.");
+    db.$disconnect()
+  });
