f8b53aec9adf1535a843a11209440a326ed87837
Toshifumi Kurosawa <kuromailserver@gmail.com>
2023-05-14 11:20:06 +0900

    clean

--- backend/src/app/middlewares/authMiddleware.js
+++ backend/src/app/middlewares/authMiddleware.js
@@ -6,10 +6,6 @@ export const authMiddleware = (req, res, next) => {
     const token = req.cookies.token;
 
     if (!token) {
-        return
-    }
-
-    if (!token) {
         return res.status(401).json({ error: "unauthenticated" });
     }
 
